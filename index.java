7a8e85f73f715765740f4593fee8fe5c482a3bc5
ammriteshh <amritesh6767@gmail.com>
2023-12-27 16:52:56 +0530

    first commit

---
+++ index.java
@@ -0,0 +1,11 @@
+class Hello {
+    public static void main(String a[]) {
+        System.out.print("Hello World");
+    }
+}
+
+// class HelloWorld {
+// public static void main(String[] args) {
+// System.out.println("Hello, World!");
+// }
+// }
