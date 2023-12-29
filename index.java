9fd059b3920d5e92008181c767693a675c7a8ed7
ammriteshh <amritesh6767@gmail.com>
2023-12-29 22:55:28 +0530

    Index

--- index.java
+++ index.java
@@ -4,3 +4,7 @@ class Hello {
     }
 }
 
+// Threre are two types of Data Types in JAVA
+// 1. Primitive(Integer[byte - 1 bytes,short - 2 bytes,int - 4 bytes,long - 8
+// bytes], Float[double - 8 bytes,float - 4 bytes], Character - 2 bytes,
+// Boolean)
