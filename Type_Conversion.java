7d6e842ff655122b949c1c839f30cd936db020bd
ammriteshh <amritesh6767@gmail.com>
2023-12-29 23:07:40 +0530

    Type Conversion

---
+++ Type_Conversion.java
@@ -0,0 +1,8 @@
+public class Type_Conversion {
+    public static void main(String args[]) {
+        // byte b = 127;
+        int a = 543;
+        byte k = (byte) a;
+        System.out.println(k);
+    }
+}
