7885a72fc0bbabc9098882a475d02aa9f7931b76
ammriteshh <amritesh6767@gmail.com>
2023-12-29 22:56:05 +0530

    Variables

--- Variables.java
+++ Variables.java
@@ -1,3 +1,16 @@
-public class Variables {
+class Hello {
+    public static void main(String a[]) {
 
+        int num1 = 9;
+        byte by = 127;
+        short sh = 32767;
+        long l = 665532;
+        float f = 5.8f;
+        double d = 4.9;
+        char c = 'k';
+        boolean b = true;
+
+        System.out.print(by); 
+        System.out.print(num1);
+    }
 }
