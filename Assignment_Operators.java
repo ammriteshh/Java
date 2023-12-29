f76ec8e63f4e04d57d8396f72953139a7d2e0667
ammriteshh <amritesh6767@gmail.com>
2023-12-29 23:15:35 +0530

    Assignment Operators

---
+++ Assignment_Operators.java
@@ -0,0 +1,12 @@
+public class Assignment_Operators {
+    public static void main(String args[]) {
+        int num1 = 7;
+        int num2 = 5;
+
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
+
+// Post-increment(num++) = fetch the value and then inrement
+// Pre-increment(++num) = increment and then fetch the value
