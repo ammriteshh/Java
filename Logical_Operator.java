d43cf886e46a097376f2c315dc5b58ab126d7549
ammriteshh <amritesh6767@gmail.com>
2023-12-29 23:27:55 +0530

    Logical Operators

---
+++ Logical_Operator.java
@@ -0,0 +1,13 @@
+public class Logical_Operator {
+    public static void main(String args[]) {
+        int x = 7;
+        int y = 5;
+        int a = 4;
+        int b = 2;
+
+        boolean result = x > y && a > b;
+        System.out.println(result);
+    }
+}
+
+// LOGOCAL = AND(&), OR(|), NOT(!)
