d6791c92810f4b3448a528edd3ec63ca781423c1
ammriteshh <amritesh6767@gmail.com>
2023-12-31 14:16:19 +0530

    Switch Statement

---
+++ Switch_Statement.java
@@ -0,0 +1,67 @@
+public class Switch_Statement {
+    public static void main(String args[]) {
+        int n = 3;
+
+        if (n == 1) {
+            System.out.println("Monday");
+        }
+        if (n == 2) {
+            System.out.println("Tuesday");
+        }
+        if (n == 3) {
+            System.out.println("Wednesday");
+        }
+        if (n == 4) {
+            System.out.println("Thrusday");
+        }
+        if (n == 5) {
+            System.out.println("Friday");
+        }
+        if (n == 6) {
+            System.out.println("Saturday");
+        }
+        if (n == 7) {
+            System.out.println("Sunday");
+        }
+
+        // if (n == 11) {
+        // System.out.println("Monday");
+        // } else if (n == 22) {
+        // System.out.println("Tuesday");
+        // } else if (n == 33) {
+        // System.out.println("Wednesday");
+        // } else if (n == 44) {
+        // System.out.println("Thrusday");
+        // } else if (n == 55) {
+        // System.out.println("Friday");
+        // } else if (n == 66) {
+        // System.out.println("Saturday");
+        // } else if (n == 77) {
+        // System.out.println("Sunday");
+        // }
+
+        // switch (n) {
+        // case 01:
+        // System.out.println("One");
+        // break;
+        // case 02:
+        // System.out.println("Two");
+        // break;
+        // case 03:
+        // System.out.println("Three");
+        // break;
+        // case 04:
+        // System.out.println("Four");
+        // break;
+        // case 05:
+        // System.out.println("Five");
+        // break;
+        // case 06:
+        // System.out.println("Six");
+        // break;
+        // case 07:
+        // System.out.println("Seven");
+        // break;
+        // }
+    }
+}
