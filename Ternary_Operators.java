a2a3d8ab26186f3eef8d86b8de98479515d47615
ammriteshh <amritesh6767@gmail.com>
2023-12-31 13:57:24 +0530

    Ternary Operators

---
+++ Ternary_Operators.java
@@ -0,0 +1,13 @@
+public class Ternary_Operators {
+    public static void main() {
+        int n = 4;
+        int result = 0;
+
+        result = n % 2 == 0 ? 10 : 20;
+
+        System.out.println(result);
+    }
+}
+
+// TERNARY = shorter form of if else
+// TERNARY OPERATOR = ?:
