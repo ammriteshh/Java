c74636a8953dedc2be03ba0bd5ec66000b0ab526
ammriteshh <amritesh6767@gmail.com>
2023-12-31 13:49:54 +0530

    If Else

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+public class ifelse {
+    public static void main(String args[]) {
+        int x = 8;
+        int y = 7;
+        int z = 6;
+
+        if (x > y && y > z) {
+            System.out.println("The value of x is greater than the values of both x and y ");
+        } else {
+            System.out.println("The value of x is not greater than the values of both y and z ");
+        }
+    }
+
+}
