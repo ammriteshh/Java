0ea014353e119acf87a6920926aa26b627ed019e
ammriteshh <amritesh6767@gmail.com>
2024-01-01 11:50:24 +0530

    For Loop

---
+++ For_Loop.java
@@ -0,0 +1,25 @@
+// public class For_Loop {
+//     public static void main(String args[]) {
+//         for (int i = 1; i <= 4; i++) {
+//             System.out.println("Hello");
+//         }
+//     }
+
+// }
+
+public class For_Loop {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Day " + i);
+
+            for (int j = 1; j <= 9; j++) {
+                System.out.println("  " + (j + 8) + " - " + (j + 9));
+            }
+        }
+    }
+
+}
+
+// in FOR LOOP we can write all the statement in one line.
+
+// We should use FOR LOOP.
