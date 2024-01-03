3cfa88b22c329fcfe80a81d11d701c24f7ff325e
ammriteshh <amritesh6767@gmail.com>
2024-01-03 21:02:23 +0530

    Object

---
+++ Object.java
@@ -0,0 +1,9 @@
+public class Object {
+    public static void main(String args[]) {
+
+    }
+}
+
+// OBJECT ORIENTED PROGRAMMING
+// Every Object has properties and behaviour
+// for Object, first we have to create a class
