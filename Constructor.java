3394c92fb0c6d37b33a114f011de4a5baa0057dd
ammriteshh <amritesh6767@gmail.com>
2024-02-17 02:28:05 +0530

    commit

---
+++ Constructor.java
@@ -0,0 +1,5 @@
+public class Constructor {
+    public static void main(String args[]){
+        Human obj = new Human();
+    }
+}
---
+++ Enumerations.java
@@ -0,0 +1,10 @@
+enum Status{
+    Running, Failed, Pending, Success;
+}
+
+public class Enumerations {
+    public static void main(String[] args) {
+        int i = 5;
+        Status s = Status.values();
+}
+}
